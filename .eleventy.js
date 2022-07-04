b2fd32d697a2efddfd0a6255eff64267cdaf49d9
Thomas Richter <thomas.karl.richter@gmail.com>
2022-07-04 17:26:05 +0200

    WIP eleventy

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,24 @@
 const markdownIt = require("markdown-it");
+const pluginSEO = require("eleventy-plugin-seo");
 
-module.exports = function(eleventyConfig) {
+module.exports = function (eleventyConfig) {
   let options = {
     html: true,
-    breaks: true,
-    linkify: true
+    breaks: false,
+    linkify: true,
   };
-
+  eleventyConfig.addPlugin(pluginSEO, require("./_data/seo.json"));
+  eleventyConfig.setLiquidOptions({
+    dynamicPartials: false,
+    root: ["_includes", "."],
+  });
+  eleventyConfig.addPassthroughCopy("assets");
   eleventyConfig.setLibrary("md", markdownIt(options));
+  return {
+    dir: {
+      input: "./",
+      output: "./_site",
+    },
+    passthroughFileCopy: true,
+  };
 };
